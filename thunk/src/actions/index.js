0899d2b97784050e1cce526010be7b7fa5401561
kirkby <kirkby@neighbor.com>
2021-02-01 21:15:18 -0700

    progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -7,31 +7,31 @@ export const CLEAR_ERROR = 'CLEAR_ERROR';
 
 // action creators!!
 // action = plain object that has a required 'type' and optional 'payload'
-export const getQuote = () => {
-
-  return (dispatch) => {
-    dispatch({ type: CLEAR_ERROR });
-    dispatch({ type: START_FETCHING });
-    // we can get here!!!!
-    // aios
-    console.log('we can call axios now!!!')
-    axios.get('https://api.kanye.rest')
-      .then(res => {
-        console.log('bk: actions/index.js: getQuote: api res: ', res)
-        dispatch({ type: QUOTE_RETRIEVED, payload: res.data.quote })
-      })
-      .catch(err => {
-        console.error('error getting a kanye quote from api: ', err)
-        dispatch({ type: FETCH_FAILED, payload: err.message })
-      })
-  }
+// export const getQuote = () => {
+// return (dispatch) => {
+export const getQuote = () => (dispatch) => {
+  dispatch({ type: CLEAR_ERROR });
+  dispatch({ type: START_FETCHING });
+  // we can get here!!!!
+  // aios
+  console.log('we can call axios now!!!')
+  axios.get('https://api.kanye.rest')
+    .then(res => {
+      console.log('bk: actions/index.js: getQuote: api res: ', res)
+      dispatch({ type: QUOTE_RETRIEVED, payload: res.data.quote })
+    })
+    .catch(err => {
+      console.error('error getting a kanye quote from api: ', err)
+      dispatch({ type: FETCH_FAILED, payload: err.message })
+    })
+}
   // ? ? ?
   // transition to isFetching: true
   // return {
   //   type: START_FETCHING
   // }
   // now let's call the api to get the quote!
-}
+// }
 
 
 //       state             action       new state
