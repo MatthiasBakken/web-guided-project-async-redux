d39646267d164b44327227a9c27b1011bf104766
kirkby <kirkby@neighbor.com>
2021-02-01 20:28:50 -0700

    progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -8,6 +8,8 @@ export const getQuote = () => {
 
 //       state             action       new state
 // noerror,noq,notfetch    getQuote()   error,noq,notfetch
+// noerror,noq,notfetch    getQuote()   noerror,hasq,notfetch
 // noerror,yesq,notfetch   getQuote()
 // ...
 // error,noq,notfetch      reload()
+// noerror,hasq,notfetch      reload()
