f88148ffa519ff83b594cbba16bd24afde68a9e9
kirkby <kirkby@neighbor.com>
2021-02-01 21:04:32 -0700

    progress

--- thunk/src/components/Quotes.js
+++ thunk/src/components/Quotes.js
@@ -19,8 +19,8 @@ const Quotes = ({ quote, isFetching, error, ...props }) => {
   }
 
   if (error) {
-    return <h2>We got an error: {error}</h2>;
-  }
+    return <h2>
+      }
 
   if (isFetching) {
     return <h2>Fetching quote for ya!</h2>;
@@ -28,18 +28,21 @@ const Quotes = ({ quote, isFetching, error, ...props }) => {
 
   return (
     <>
-      <h2>Kanye says: {quote}</h2>
-      <button onClick={handleGetQuote}>Get new quote</button>
-    </>
+        <h2>Kanye says: {quote}</h2>
+        {
+          error && <h2>We got an error: {error}</h2>
+        }
+        <button onClick={handleGetQuote}>Get new quote</button>
+      </>
   );
 };
 
 const mapStateToProps = state => {
   return {
-    quote: state.quote,
+        quote: state.quote,
     isFetching: state.isFetching,
     error: state.error
   };
 };
 
-export default connect(mapStateToProps, { getQuote })(Quotes);
+export default connect(mapStateToProps, {getQuote})(Quotes);
