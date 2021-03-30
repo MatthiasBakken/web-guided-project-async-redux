fb54e08cd8cea516ef0c4ecfc8cccd574d5860be
kirkby <kirkby@neighbor.com>
2021-03-29 20:39:36 -0600

    progress

--- thunk/src/App.js
+++ thunk/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Comic from './components/Comic';
 
 import './styles.css';
 
 function App() {
+  const handleSetComicTitle = e => {
+    e.preventDefault();
+
+  }
   return (
     <div className="App">
       <h1>xkcd</h1>
       <Comic />
+      <button onClick={handleSetComicTitle}>set commic title</button>
     </div>
   );
 }
 
-export default App;
+export default connect(() => { return {} }, { updateComicTitle })(App);
