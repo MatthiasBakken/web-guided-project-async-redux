0c3450de22f52e103bfae363196b08fbc56563ab
kirkby <kirkby@neighbor.com>
2021-03-29 21:03:00 -0600

    progress

--- thunk/src/App.js
+++ thunk/src/App.js
@@ -11,10 +11,17 @@ function App(props) {
     e.preventDefault();
     props.updateComicTitle('this is my new title');
   }
+
+  const handleGetComic = e => {
+    e.preventDefault();
+    props.getLatestComic();
+  }
+
   return (
     <div className="App">
       <h1>xkcd</h1>
       <Comic />
+      <button onClick={handleGetComic}>get comic</button>
       <button onClick={handleSetComicTitle}>set comic title</button>
     </div>
   );
