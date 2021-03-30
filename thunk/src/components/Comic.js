2ca6d6cd0db463dc5b3dcff70721fbc4bae3d0fa
kirkby <kirkby@neighbor.com>
2021-03-29 20:35:11 -0600

    progress

--- thunk/src/components/Comic.js
+++ thunk/src/components/Comic.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const Comic = props => {
   return (<div>
@@ -6,4 +7,10 @@ const Comic = props => {
   </div>)
 }
 
-export default Comic;
+const mapStateToProps = state => {
+  return {
+    comicTitle: state.comicTitle
+  }
+}
+
+export default connect(mapStateToProps, {})(Comic);
