e407692530df727804e9cb476ece67b4fdf34562
kirkby <kirkby@neighbor.com>
2021-03-29 20:42:56 -0600

    progress

--- thunk/src/App.js
+++ thunk/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Comic from './components/Comic';
+import { updateComicTitle } from './actions';
 
 import './styles.css';
 
