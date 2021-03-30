8f48b38249c9fe100f3dd3b44f84551e551f3ab3
kirkby <kirkby@neighbor.com>
2021-03-29 21:00:54 -0600

    progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,4 +1,6 @@
 export const UPDATE_COMIC_TITLE = 'UPDATE_COMIC_TITLE';
+export const START_FETCHING_COMIC = 'START_FETCHING_COMIC';
+export const END_FETCHING_COMIC = 'END_FETCHING_COMIC';
 
 export const updateComicTitle = (newTitle) => {
   return (dispatch) => {
