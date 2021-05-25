7b89bea945fe1ca4f754a37ce3d8bd7fa64be56a
kirkby <kirkby@neighbor.com>
2021-05-24 20:34:04 -0600

    update progress!!

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,10 +1,16 @@
 // https://animechan.vercel.app/api/random
+import axios from 'axios';
 export const SET_FETCHING_QUOTE = 'SET_FETCHING_QUOTE';
 
 
 export const getRandomQuote = () => {
   // transtion state to isFetching = true
-  return {
-    type: SET_FETCHING_QUOTE
-  }
+  dispatch({ type: SET_FETCHING_QUOTE });
+
+  axios.get('https://animechan.vercel.app/api/random')
+    .then(res => {
+      // got a quote!
+      // state transition
+      // dispatch({ type: 'QUOTE_FETCH_SUCCESS', payload: res.data.})
+    })
 }
