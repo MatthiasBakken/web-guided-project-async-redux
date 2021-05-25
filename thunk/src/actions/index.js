241345e5e038353ab7a1338398b1a2101928f7ef
kirkby <kirkby@neighbor.com>
2021-05-24 21:00:22 -0600

    update progress!!

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 export const SET_FETCHING_QUOTE = 'SET_FETCHING_QUOTE';
 
 
-export const getRandomQuote = () => (dispatch) => {
-  // transtion state to isFetching = true
-  dispatch({ type: SET_FETCHING_QUOTE });
+export const getRandomQuote = () => {
+  return (dispatch) => {
+    // transtion state to isFetching = true
+    dispatch({ type: SET_FETCHING_QUOTE });
 
-  axios.get('https://animechan.vercel.app/api/random')
-    .then(res => {
-      console.log('bk: actions/: getRandomQuote: res: ', res)
-      // got a quote!
-      // state transition
-      // dispatch({ type: 'QUOTE_FETCH_SUCCESS', payload: res.data.})
-    })
+    axios.get('https://animechan.vercel.app/api/random')
+      .then(res => {
+        console.log('bk: actions/: getRandomQuote: res: ', res)
+        // got a quote!
+        // state transition
+        // dispatch({ type: 'QUOTE_FETCH_SUCCESS', payload: res.data.})
+      })
+  }
 }
